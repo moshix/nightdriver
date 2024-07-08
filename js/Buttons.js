f951d13bd6e83e24d0d1dbfa2a45b850ce53f183
moshix <moshix@gmail.com>
2024-07-08 19:36:18 +0200

    Update Buttons.js

--- js/Buttons.js
+++ js/Buttons.js
@@ -46,7 +46,7 @@ export default class Buttons {
 
     this.addButton(
       "camera",
-      "driver",
+      "hood",
       ["driver", "hood", "rear", "backseat", "chase", "aerial", "satellite"],
       (value) => `
       <div class='label'>camera</div>
